bbec55845c756547233bd84042c9e92c589b263d
jlaffaye <jlaffaye@FreeBSD.org>
2011-04-17 17:33:07 +0100

    Refactor do_jobs().
    Do jobs that were waiting for orther jobs to finish.

--- do.c
+++ do.c
@@ -36,97 +36,155 @@ struct proc_info {
 	struct node *node;
 };
 
+struct state {
+	struct node *jobs;
+	unsigned int num_jobs;
+	unsigned int num_done;
+	int num_proc;
+	int num_active;
+	struct proc_info *pi;
+	struct pollfd *pfd;
+};
+
+static int
+start_job(struct state *s)
+{
+	struct node *n;
+	int fd;
+	int i;
+
+	/* find the first empty slot */
+	for (i = 0; i < s->num_proc; i++)
+		if (s->pfd[i].fd == -1)
+			break;
+	assert(s->pfd[i].fd == -1);
+
+	n = s->jobs;
+	if ((s->pi[i].pid = popen2(n->cmd, i, &fd)) < 0) {
+		perror("popen2()");
+		return 1;
+	}
+
+	s->pfd[i].fd = fd;
+	s->pi[i].node = n;
+	s->num_active++;
+
+	DL_DELETE(s->jobs, n);
+
+	printf("[%d/%d] %s\n", s->num_done + s->num_active, s->num_jobs, n->name);
+	return 0;
+}
+
+static int
+finish_job(struct state *s, int i)
+{
+	struct node *n;
+	struct node *np;
+	int ret;
+	size_t j;
+
+	ret = pclose2(s->pi[i].pid, s->pfd[i].fd);
+
+	s->pfd[i].fd = -1;
+	s->num_active--;
+
+	if (ret != 0) {
+		fprintf(stderr, "*** Error code %d\n", ret);
+		return 1;
+	}
+
+	s->num_done++;
+
+	/*
+	 * Add jobs that were waiting for the current job to finish
+	 */
+	n = s->pi[i].node;
+	for (j = 0; j < n->parents.len; j++) {
+		np = n->parents.nodes[j];
+		np->waiting--;
+		if (np->waiting == 0 && np->todo == 1)
+			DL_APPEND(s->jobs, np);
+	}
+	return 0;
+}
+
+static int
+read_pipe(struct state *s, int i)
+{
+	ssize_t sz;
+	char buf[8192];
+
+	if ((sz = read(s->pfd[i].fd, buf, sizeof(buf))) < 0) {
+		perror("read()");
+		finish_job(s, i);
+		return 1;
+	}
+
+	/*
+	 * If `sz' is 0, this is EOF so the process is terminated
+	 */
+	if (sz == 0)
+		return finish_job(s, i);
+	else
+		write(1, buf, sz); /* TODO: store in pi[i].buf */
+
+	return 0;
+}
+
+static int
+ipc(struct state *s)
+{
+	/* TODO */
+	return 0;
+}
+
 int
 do_jobs(int num_proc)
 {
-	struct node *jobs;
-	struct node *n;
-	unsigned int num_jobs;
-	unsigned int num_done = 0;
-	int num_active = 0;
+	struct state s = { NULL, 0, 0, num_proc, 0, NULL, NULL};
 	int i;
-	int fd;
-	ssize_t sz;
-	char buf[8192];
-	struct proc_info *pi;
-	struct pollfd *pfd;
 	int error = 0;
 
-	num_jobs = graph_compute(&jobs);
+	s.num_jobs = graph_compute(&s.jobs);
 
-	pi = malloc(num_proc * sizeof(struct proc_info));
+	s.pi = malloc(num_proc * sizeof(struct proc_info));
 
 	/* `num_proc' pipes + 1 unix socket*/
-	pfd = malloc((num_proc + 1) * sizeof(struct pollfd));
-	for (i = 0; i < (num_proc + 1); i++) {
-		pfd[i].fd = -1;
-		pfd[i].events = POLLIN;
+	s.pfd = malloc((s.num_proc + 1) * sizeof(struct pollfd));
+	for (i = 0; i < (s.num_proc + 1); i++) {
+		s.pfd[i].fd = -1;
+		s.pfd[i].events = POLLIN;
 	}
 
 	/*
 	 * Iterate as long as there are jobs to do/being done.
 	 * If there is an error, we still want to wait for running jobs to finish.
 	 */
-	while (num_done != num_jobs && (error == 0 || num_active > 0)) {
+	while (s.num_done != s.num_jobs && (error == 0 || s.num_active > 0)) {
 
 		/*
 		 * Launch new jobs if we have empty slots and if we have pending jobs.
 		 * If there is an error, we do not want to launch new jobs.
 		 */
-		while (num_active <= num_proc && jobs != NULL && error == 0) {
-			/* find the first empty slot */
-			for (i = 0; i < num_proc; i++)
-				if (pfd[i].fd == -1)
-					break;
-			assert(pfd[i].fd == -1);
+		while (s.num_active <= s.num_proc && s.jobs != NULL && error == 0)
+			start_job(&s);
 
-			n = jobs;
-			if ((pi[i].pid = popen2(n->cmd, i, &fd)) < 0)
-				err(1, "popen2()");
+		assert(s.num_active > 0);
 
-			pfd[i].fd = fd;
-			pi[i].node = n;
-			num_active++;
-
-			DL_DELETE(jobs, n);
-
-			printf("[%d/%d] %s\n", num_done + num_active, num_jobs, n->name);
-		}
-
-		assert(num_active > 0);
-
-		/* poll running jobs */
-		if (poll(pfd, num_proc + 1, -1) < 0)
+		/* poll running jobs + unix socket */
+		if (poll(s.pfd, num_proc + 1, -1) < 0)
 			err(1, "poll()");
 
-		for (i = 0; i < num_proc; i++) {
-			if (pfd[i].revents & POLLIN) {
-				if ((sz = read(pfd[i].fd, buf, sizeof(buf))) < 0) {
-					error = 1;
-					perror("read()");
-				}
-				/*
-				 * If `sz' is 0, this is EOF so the process is terminated
-				 */
-				if (sz <= 0) {
-					/* TODO: report error code != 0 */
-					if (pclose2(pi[i].pid, pfd[i].fd) != 0) {
-						error = 1;
-					}
-					pfd[i].fd = -1;
-					num_active--;
-					num_done++;
-				}
-				write(1, buf, sz); /* TODO: store in pi[i].buf */
-			}
-		}
+		for (i = 0; i < num_proc; i++)
+			if (s.pfd[i].revents & POLLIN)
+				error += read_pipe(&s, i);
 
 		/* special case for the unix socket */
-		if (pfd[num_proc].revents & POLLIN) {
-		}
+		if (s.pfd[num_proc].revents & POLLIN)
+			ipc(&s);
 	}
 
-	free(pfd);
-	free(pi);
+	free(s.pfd);
+	free(s.pi);
 	return error;
 }
