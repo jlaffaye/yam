9c2331e967c66088c177f64d9b8a54a6af82ef82
jlaffaye <jlaffaye@FreeBSD.org>
2011-04-17 10:56:06 +0100

    Simplify the wrapper.
    Do not report files which are opened for write.

--- wrapper/wrapper.c
+++ wrapper/wrapper.c
@@ -7,11 +7,6 @@
 
 void send_data_ipc(void);
 
-//FILE * fopen(const char *, const char *);
-//int open(const char *, int, ...);
-int close(int);
-ssize_t read(int, void *, size_t);
-
 static int send_data_ipc_registered = 0;
 
 FILE *
@@ -30,34 +25,11 @@ fopen(const char *path, const char *mode)
 
 	fp = func(path, mode);
 
-	if (fp != NULL) {
-		/* Do not bother with writes */
-		if (mode[0] == 'r' || mode[1] == '+') {
-			printf("fopen: %d -> %s\n", fileno(fp), path);
-		}
+	if (fp != NULL && mode[0] == 'r') {
+		printf("fopen: %s (%s)\n", path, mode);
 	}
 
 	return fp;
-}
-
-int
-fclose(FILE *fp)
-{
-	static int (*func)(FILE *);
-	int fd;
-	int r;
-
-	if (func == NULL)
-		func = dlsym(RTLD_NEXT, "fclose");
-
-	fd = fileno(fp);
-	r = func(fp);
-
-	if (r == 0) {
-		printf("fclose: %d\n", fd);
-	}
-
-	return r;
 }
 
 int
@@ -85,44 +57,12 @@ open(const char *path, int flags, ...)
 	} else
 		fd = func(path, flags);
 
-	if (fd > 0)
-		/* Do not bother with writes */
-		if ((flags & O_RDONLY) == O_RDONLY || (flags & O_RDWR) == O_RDWR)
-			printf("open: %d -> %s\n", fd, path);
+	if (fd > 0 && ((flags & O_RDONLY) == O_RDONLY || (flags & O_RDWR) == O_RDWR) &&
+		(flags & O_CREAT) != O_CREAT && (flags & O_TRUNC) != O_TRUNC) {
+		printf("open: %d -> %s\n", fd, path);
+	}
 
 	return fd;
-}
-
-int
-close(int fd)
-{
-	static int (*func)(int);
-	int r;
-
-	if(func == NULL)
-		func = dlsym(RTLD_NEXT, "close");
-
-	r = func(fd);
-
-	if (r == 0) {
-		printf("close: %d\n", fd);
-	}
-
-	return r;
-}
-
-ssize_t
-read(int fd, void *buf, size_t nbytes)
-{
-	ssize_t (*func)(int, void *, size_t) = NULL;
-
-	if (func == NULL) {
-		func = dlsym(RTLD_NEXT, "read");
-	}
-
-	printf("read: %d\n", fd);
-
-	return func(fd, buf, nbytes);
 }
 
 void
